b6b6efd698a82c87f7d29ac1af142e48e1600471
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-21 00:39:46 +0600

    two sum added

--- two_sum.js
+++ two_sum.js
@@ -1,1 +1,13 @@
-console.log("hello");
+var twoSum = function (nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
+};
+
+console.log(twoSum([4, 5, 4, 3, 7], 8));
+
+// output: [ 0, 2 ]
